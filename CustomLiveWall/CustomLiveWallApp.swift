ac4b225a1d4f9cb87c9e08b5e1b35b24c82a8f2c
sohanparves <rafsanahamedsohan@gmail.com>
2026-08-16 20:05:26 +0600

    Initial Commit

---
+++ CustomLiveWall/CustomLiveWallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLiveWallApp.swift
+//  CustomLiveWall
+//
+//  Created by Sohan Parves on 16/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct CustomLiveWallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
